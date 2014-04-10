7fdb1a42a80f0206e74109aedfe4fa9056ae2e7a
Dolgikh <dolgikh@Andreys-Mac-mini-2.local>
2014-04-10 17:38:12 +0300

    Initial Commit

---
+++ SecLesson/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SecLesson
+//
+//  Created by Dolgikh on 4/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
